dcf4e3f924c309a53a7333cd5a229847e8f14e02
kashif <blackwatch4321@gmail.com>
2025-12-28 11:41:53 +0500

    changes

--- src/components/Projects-section/Projects.jsx
+++ src/components/Projects-section/Projects.jsx
@@ -1,7 +1,23 @@
+import "./Projects.css";
+import cardImg from "../../assets/tame-impala.jpeg";
+
 const Projects = () => {
   return (
     <div>
-      <h1>Projects</h1>
+      <h1 className="p-h">Projects</h1>
+      <div className="project-card">
+        <div className="card-text">
+          <h3>Project Name</h3>
+          <p>
+            I created this personal project in order to show how to create an
+            interface in Figma using a portfolio as an example.
+          </p>
+          <button>View Project</button>
+        </div>
+        <div className="card-img">
+          <img src={cardImg} alt="tame-impala" />
+        </div>
+      </div>
     </div>
   );
 };
